c023549d121b5c217eefe3655cc84e6bac79e121
Teevarapat <threevarapat@hotmail.com>
2014-11-19 14:01:25 +0700

    มีหน้าต่างโชว์แล้วแต่ออกไม่ได้ครับ TT

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from gamelib import SimpleGame
 class SquashGame(SimpleGame):
     pass
  
+    def init(self):
+    	super(SquashGame,self).init()
+
 def main():
-    game = SquashGame()
+    game = SquashGame("squash")
     game.run()
  
 if __name__ == '__main__':
